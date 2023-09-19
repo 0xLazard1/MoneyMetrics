1c24e6c4b4ddc441a70f9cce5d71b3242fe61092
0xLazard1 <DevLazard@proton.me>
2023-09-19 05:36:26 +0300

    update APR.js, APY.js and index.html

--- JS/StakingMode/FunctionCalculStaking/APR/APR.js
+++ JS/StakingMode/FunctionCalculStaking/APR/APR.js
@@ -102,3 +102,4 @@ export const APR = () => {
 }
 
 
+
--- JS/StakingMode/FunctionCalculStaking/APY/APY.js
+++ JS/StakingMode/FunctionCalculStaking/APY/APY.js
@@ -6,8 +6,8 @@ export const APY = () => {
         element.innerHTML = `${value} : <span style="color: ${color}">${suffix}</span>`;
     };
 
-    const [quantitestaking, pourcentageapy, commission, frequency] =
-        [elements.Input_QuantiteStaking, elements.Input_PourcentageAPR, elements.Input_CommisionStaking, elements.Input_CapitalizationFrequency
+    const [quantitestaking, pourcentageapy, commission, frequency, temps] =
+        [elements.Input_QuantiteStaking, elements.Input_PourcentageAPR, elements.Input_CommisionStaking, elements.Input_CapitalizationFrequency, elements.Input_RangeTemps
         ]
             .map(el => el.value);
 
@@ -40,14 +40,17 @@ export const APY = () => {
                 n = 1;
         }
 
-        const APYValue = (1 + (numeriquePourcentageapy / n)) ** n - 1; // Formule pour l'APY
+        //add the variable to calculate automatically nettokengagné with autocompound
+        const APYValue = (1 + (numeriquePourcentageapy / n)) ** (n * temps) - 1; // Formule pour l'APY
         const BrutTokenGagné = numeriqueQuantitestaking * APYValue;   // Total des tokens gagnés
-        const Commission = BrutTokenGagné * numeriqueCommission / 100;       // Commission basée sur le pourcentage
-        const NetTokenGagné = BrutTokenGagné - Commission;              // Tokens après soustraction de la commission
+        const commission = BrutTokenGagné * numeriqueCommission / 100;       // Commission basée sur le pourcentage
+        const NetTokenGagné = BrutTokenGagné - commission;              // Tokens après soustraction de la commission
         const TokenBenefice = NetTokenGagné + numeriqueQuantitestaking;       // Total des tokens après ajout du bénéfice net
 
-        displayResults(elements.ResultatCalculStaking, 'Tokens obtenus avec capitalisation', 'green', `${NetTokenGagné.toFixed(2)}`)
+        //change the display because very high number
+        displayResults(elements.ResultatCalculStaking, 'Tokens obtenus avec capitalisation', 'green', `${NetTokenGagné.toFixed(1)}`)
+
         displayResults(elements.WalletBeneficeStaking, 'Total de tokens après staking avec capitalisation', 'green', `${TokenBenefice.toFixed(2)}`)
-        displayResults(elements.CommissionStaking, 'Tokens déduits en commission avec capitalisation', 'red', `${Commission.toFixed(2)}`)
+        displayResults(elements.CommissionStaking, 'Tokens déduits en commission avec capitalisation', 'red', `${commission.toFixed(2)}`)
     }
 }
